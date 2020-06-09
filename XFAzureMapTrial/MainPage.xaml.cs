0c44fcaa982f0f68719387fb92f3232a6c44f9ed
Goutham R <gouthamr2896@gmail.com>
2020-06-09 12:15:20 +0530

    Consuming Search Azure service.

--- XFAzureMapTrial/MainPage.xaml.cs
+++ XFAzureMapTrial/MainPage.xaml.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using XFAzureMapTrial.Models;
+using XFAzureMapTrial.Services;
 
 namespace XFAzureMapTrial
 {
@@ -13,9 +16,46 @@ namespace XFAzureMapTrial
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        readonly string azureKey = "";
+
         public MainPage()
         {
             InitializeComponent();
+            Device.BeginInvokeOnMainThread(async () => {
+                await SearchForAddress("400 Broad St, Seattle, WA 98109&typeahead=true");
+            });
         }
+
+        public async Task SearchForAddress(string address)
+        {
+            StringBuilder stringUri = new StringBuilder();
+            stringUri.Append("https://us.atlas.microsoft.com/search/address/json?");
+            stringUri.Append("api-version=1.0&subscription-key=");
+            stringUri.Append(azureKey);
+            stringUri.Append("&query=");
+            stringUri.Append(address);
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                try {
+                    var result = await RequestProvider.Instance.GetAsync<Temperatures>(stringUri.ToString());
+                    if (result != null && result.Results != null && result.Results.Count > 0)
+                    {
+                        foreach (Result res in result.Results)
+                            AddPinToMap(res.Position, res.Address.FreeformAddress);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+
+        private void AddPinToMap(Position pos, string address)
+        {
+            //TODO:
+        }
+
     }
 }
