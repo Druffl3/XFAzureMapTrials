8e26f9d852f7623c42d1aaeb9fd18a9de5a2f4b4
Goutham R <gouthamr2896@gmail.com>
2020-06-09 12:12:01 +0530

    Setup RequisteProvider for HTTP calls.

---
+++ XFAzureMapTrial/Services/RequestProvider.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace XFAzureMapTrial.Services
+{
+    public sealed class RequestProvider
+    {
+        private readonly JsonSerializerSettings _serializerSettings;
+        private RequestProvider()
+        {
+            _serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+            _serializerSettings.Converters.Add(new StringEnumConverter());
+        }
+
+        public static RequestProvider _instance;
+
+        public static RequestProvider Instance {
+            get {
+                if (_instance == null)
+                    _instance = new RequestProvider();
+                return _instance;
+            }
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string uri)
+        {
+
+            HttpClient httpClient = CreateHttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            TResult result = await Task.Run(() =>
+                JsonConvert.DeserializeObject<TResult>(serialized, _serializerSettings));
+
+            return result;
+        }
+
+        public async Task<TResult> PostAsync<TResult>(string uri, TResult data, string header = "")
+        {
+            HttpClient httpClient = CreateHttpClient();
+
+            if (!string.IsNullOrEmpty(header))
+            {
+                AddHeaderParameter(httpClient, header);
+            }
+
+            var temp = JsonConvert.SerializeObject(data);
+            StringContent content = new StringContent(temp);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PostAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+            TResult result = await Task.Run(() =>
+            JsonConvert.DeserializeObject<TResult>(serialized, _serializerSettings));
+            return result;
+        }
+
+        private void AddHeaderParameter(HttpClient httpClient, string parameter)
+        {
+            if (httpClient == null)
+                return;
+
+            if (string.IsNullOrEmpty(parameter))
+                return;
+
+            httpClient.DefaultRequestHeaders.Add(parameter, Guid.NewGuid().ToString());
+        }
+
+        private HttpClient CreateHttpClient()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
+    }
+}
