d42cb738861c8fe68f7be9007a26c2cc3a2dda01
Goutham R <gouthamr2896@gmail.com>
2020-06-18 11:12:10 +0530

    Added AppConstants.cs Added UWP project.

---
+++ XFAzureMapTrial/AppConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFAzureMapTrial
+{
+    public sealed class AppConstants
+    {
+        public static string SubscriptionKey { get; } = "";
+        public static string EndPoint { get; } = "";
+    }
+}
